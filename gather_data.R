ba60ca0be85b4d605f3b14785e4095697a56689c
billzichos <billzichos@hotmail.com>
2015-10-02 21:38:36 -0400

    cleaned up the gather data file

--- gather_data.R
+++ gather_data.R
@@ -11,10 +11,3 @@ source("~/GitHub/Get-Raw-Data/download.R")
 downloadMultKaggleZip("grasp-and-lift-eeg-detection","test.zip")
 downloadSingleKaggleZip("grasp-and-lift-eeg-detection","sample_submission.csv.zip","sample_submission.csv")
 downloadMultKaggleZip("grasp-and-lift-eeg-detection","train.zip")
-
-datalist <- list(
-        train=read.csv(
-                paste(wd,"\\",trnFile, sep=""), header=TRUE, as.is=TRUE), 
-        test=read.csv(
-                paste(wd,"\\",tstFile, sep=""), header=TRUE, as.is=TRUE)
-        )
